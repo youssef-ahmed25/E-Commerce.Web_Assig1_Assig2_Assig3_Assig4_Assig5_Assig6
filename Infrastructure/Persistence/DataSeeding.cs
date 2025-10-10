5c60c7e7be77634f5fbbb9be297c6dac96bfd4bd
youssef-ahmed25 <youssefahmed56432@gmail.com>
2025-10-10 18:52:36 +0300

    ECommerceApi

--- Infrastructure/Persistence/DataSeeding.cs
+++ Infrastructure/Persistence/DataSeeding.cs
@@ -1,10 +1,12 @@
 ﻿using DomainLayer.Contracts;
+using DomainLayer.Models;
 using Microsoft.EntityFrameworkCore;
 using Persistence.Data.Contexts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Persistence
@@ -13,9 +15,48 @@ namespace Persistence
     {
         public void DataSeed()
         {
-            if(_dbContext.Database.GetPendingMigrations().Any())
+            try
             {
-                _dbContext.Database.Migrate();
+                if (_dbContext.Database.GetPendingMigrations().Any())
+                {
+                    _dbContext.Database.Migrate();
+                }
+                if (!_dbContext.ProductBrands.Any())
+                {
+                    var productBrandData = File.ReadAllText(@"..\Infrastructure\Persistence\Data\DataSeed\brands.json");
+                    var productBrands = JsonSerializer.Deserialize<List<ProductBrand>>(productBrandData);
+                    if (productBrands is not null && productBrands.Any())
+                    {
+                        _dbContext.ProductBrands.AddRange(productBrands);
+                    }
+
+                }
+                if (!_dbContext.ProductTypes.Any())
+                {
+                    var productTypeData = File.ReadAllText(@"../Infrastructure/Persistence/Data/DataSeed/types.json");
+                    var productTypes = JsonSerializer.Deserialize<List<ProductType>>(productTypeData);
+                    if (productTypes is not null && productTypes.Any())
+                    {
+                        _dbContext.ProductTypes.AddRange(productTypes);
+                    }
+
+                }
+                if (!_dbContext.Products.Any())
+                {
+                    var productsData = File.ReadAllText(@"../Infrastructure/Persistence/Data/DataSeed/products.json");
+                    var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+                    if (products is not null && products.Any())
+                    {
+                        _dbContext.Products.AddRange(products);
+                    }
+
+                }
+                _dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                //to do
             }
         }
     }
